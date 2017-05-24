75572592b5f976b43ff49d2c0a10f1cc57d7ff68
NSD <metall112@bk.ru>
2017-05-24 16:53:33 +0300

    ready to deploy

---
+++ src/main/webapp/js/index_init.js
@@ -0,0 +1,18 @@
+/**
+ * Created by NSD on 24.05.17.
+ */
+$(document).ready(function () {
+
+
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    // $('.button-collapse').sideNav();
+
+    setInterval(function() {
+        $('.carousel').carousel('next');
+    }, 5000);
+
+
+
+
+
+});
