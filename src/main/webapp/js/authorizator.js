dd86570a327cb7079a5128a14391b6bb746567ea
NSD <metall112@bk.ru>
2017-06-05 18:55:40 +0300

    fix url encoded

--- src/main/webapp/js/authorizator.js
+++ src/main/webapp/js/authorizator.js
@@ -4,6 +4,18 @@
 
 
 
+
+function getCurentUser() {
+
+    $.get("/API/Auth",{user:"user"},function (data) {
+
+        var user = JSON.parse(data);
+        console.log(data);
+    });
+
+
+}
+
 function check_user_role() {
 
 
