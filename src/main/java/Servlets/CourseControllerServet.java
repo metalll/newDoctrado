59b9558c556664edaf0ef7ff41b139a262a47661
NSD <metall112@bk.ru>
2017-06-12 02:51:53 +0300

    fix getting course

--- src/main/java/Servlets/CourseControllerServet.java
+++ src/main/java/Servlets/CourseControllerServet.java
@@ -244,7 +244,7 @@ try {
 
 
                PrintWriter out = resp.getWriter();
-               out.write(String.valueOf(tempCourse.getId());
+               out.write(String.valueOf(tempCourse.getId()));
                out.flush();
                out.close();
 
