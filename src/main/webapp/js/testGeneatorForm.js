ff452f200471311846ffca6397bf8902e9b95168
NSD <metall112@bk.ru>
2017-06-12 03:49:58 +0300

    fix getting course

--- src/main/webapp/js/testGeneatorForm.js
+++ src/main/webapp/js/testGeneatorForm.js
@@ -427,30 +427,7 @@ function call1() {
         }
     });
 }
-tinymce.init({
-    selector: 'textarea',
-    height: 500,
-    entity_encoding : "utf-8",        theme: 'modern',
-    plugins: [
-        'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-        'searchreplace wordcount visualblocks visualchars code fullscreen',
-        'insertdatetime media nonbreaking save table contextmenu directionality',
-        'emoticons template paste textcolor colorpicker textpattern imagetools codesample toc'
-    ],
-    toolbar1: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-    toolbar2: 'print preview media | forecolor backcolor emoticons | codesample',
-    image_advtab: true,
-    language: "ru",
-    translate_mode: true,
-    templates: [
-        {title: 'Test template 1', content: 'Test 1'},
-        {title: 'Test template 2', content: 'Test 2'}
-    ],
-    content_css: [
-        '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-        '//www.tinymce.com/css/codepen.min.css'
-    ]
-});
+
 
 
 function callReg1(accept) {
